655cd46c0b174cc6ab95fc66846138171bd771d3
kangyouyin <kangyouyin@jd.com>
2019-12-24 17:13:49 +0800

    first commit

---
+++ src/test/java/com/kyy/oa_project/OaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyy.oa_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
